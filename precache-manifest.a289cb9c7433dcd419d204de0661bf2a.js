ef794f300b274dc181b6d6b6ef2993c1f052c16a
henansong <github@nansenho.com>
2021-01-14 16:32:41 +0900

    deploy

---
+++ precache-manifest.a289cb9c7433dcd419d204de0661bf2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aab5d90548195a883e3792440a9594ca",
+    "url": "/money-website/QRcode.png"
+  },
+  {
+    "revision": "5335e78c5fb7f0e089c1",
+    "url": "/money-website/css/app.9e84fad7.css"
+  },
+  {
+    "revision": "cef7f5933da881da30e06b97b7d5c786",
+    "url": "/money-website/index.html"
+  },
+  {
+    "revision": "5335e78c5fb7f0e089c1",
+    "url": "/money-website/js/app.9d5a9178.js"
+  },
+  {
+    "revision": "62733693d151b3d27b1e",
+    "url": "/money-website/js/chunk-vendors.034b6317.js"
+  },
+  {
+    "revision": "6f94a71a55b472b5d6172ab127074578",
+    "url": "/money-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-website/robots.txt"
+  }
+]);
